2c9206dd549cf4ccb3dbb253cd15787f3ee42187
DULANGHAI <604857875@qq.com>
2017-12-25 16:48:51 +0800

    提交人：王林 提交事项：基本框架

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>recommend页面</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>search页面</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
